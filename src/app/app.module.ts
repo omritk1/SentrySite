ae7bafad2fb496dd904b3a6d62730a1882496971
omritk1 <galili.omri@gmail.com>
2018-03-22 18:57:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { ProjectGalleryComponent } from './project-gallery/project-gallery.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeBannerComponent,
+    ProjectGalleryComponent,
+    SubHeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
